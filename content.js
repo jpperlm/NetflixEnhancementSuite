caf94ca8f4582029d076e52d2de4f8ba1eeb531b
Jason Perlman <jasonpperlman@gmail.com>
2019-03-03 08:14:30 -0500

    Same functionallity can be achieved simply through stylesheets

--- content.js
+++ content.js
@@ -1,16 +1,18 @@
-window.onscroll = function () {
-  remove_visible();
-};
-window.onload = function () {
-  remove_visible();
-};
-function remove_visible(){
-  const class_names = ['billboard-row','bigRow'];
-  class_names.forEach(name=>{
-    let list = document.getElementsByClassName(name);
-    for (let item of list) {
-      item.style.display='none';
-    }
-  });
-}
-remove_visible();
+// debugger
+// window.onscroll = function () {
+//   remove_visible();
+// };
+// window.onload = function () {
+//   remove_visible();
+// };
+// function remove_visible(){
+//   debugger
+//   const class_names = ['billboard-row','bigRow'];
+//   class_names.forEach(name=>{
+//     let list = document.getElementsByClassName(name);
+//     for (let item of list) {
+//       item.style.display='none';
+//     }
+//   });
+// }
+// remove_visible();
